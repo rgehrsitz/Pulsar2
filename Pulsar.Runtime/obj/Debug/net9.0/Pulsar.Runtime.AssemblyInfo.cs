361fe32ae66cf6020122cb2e9d59f6f3240195be
robertg@digwave.com <Robert>
2025-01-06 16:09:04 -0500

    Add global.json and system_config.yaml; update project dependencies and configurations

--- Pulsar.Runtime/obj/Debug/net9.0/Pulsar.Runtime.AssemblyInfo.cs
+++ Pulsar.Runtime/obj/Debug/net9.0/Pulsar.Runtime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pulsar.Runtime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8723ab7f0d9dc68517e660730dde54baa1220541")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pulsar.Runtime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pulsar.Runtime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Pulsar.Tests/obj/Debug/net9.0/Pulsar.Tests.AssemblyInfo.cs
+++ Pulsar.Tests/obj/Debug/net9.0/Pulsar.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pulsar.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8723ab7f0d9dc68517e660730dde54baa1220541")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pulsar.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pulsar.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
